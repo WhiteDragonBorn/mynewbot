da42dd704b8f6ef23580ec33f6366f7bd033b26f
WhiteDragonBorn <shimanoee@yandex.ru>
2021-05-16 21:25:29 +0300

    Теперь бот может отвечать на сообщение "саня"

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import bot
 
 client = commands.Bot(command_prefix='.')
 
@@ -9,4 +10,18 @@ async def on_ready():
     print('Bot is ready')
 
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+        if message.content == 'саня':
+            await message.channel.send('саня')
+
+
+client = MyClient()
 client.run('ODQzNTM1NDQxNTM4MTg3MzE1.YKFRmQ.ZV_LJj9EQTUAtfQCZV8Lfk0Nxb4')
