7210530b2eccad81ae920ace2f54f232e5ea696c
WhiteDragonBorn <shimanoee@yandex.ru>
2021-05-17 18:56:09 +0300

    Доделал чуть бота, не забудьте токен

--- Bot.py
+++ Bot.py
@@ -1,32 +1,63 @@
 import discord
 from discord.ext import commands
+import random
 from discord.ext.commands import bot
+
 
 client = commands.Bot(command_prefix='.')
 
+@client.event
+async def on_ready():
+    print('Bot is ready') # Если всё загрузилось правильно - вывыдет ф-ию принта
 
+# class MyClient(discord.Client):
 # @client.event
-# async def on_ready():
-#     print('Bot is ready')
+# async def on_ready(self):
+#     print(f'Logged in as {self.user} (ID: {self.user.id})')
+#     print('------')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+#
+# @client.event
+# async def on_member_join(member):
+#     print(f'{member} has joined a server.')
+#
+#
+# @client.event
+# async def on_member_remove(member):
+#     print(f'{member} has leaved a server.')
+#
+#
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-        if message.content == 'Павло':
-            await message.channel.send('саня')
-
-        if message.content == 'пидор':
-            await message.channel.send('сам такой!')
-
-    async def pog(ctx):
-        await ctx.channel.send('FUCK YOU')
+#
+# async def on_message(self, message):
+#     # don't respond to ourselves
+#     if message.author == self.user:
+#         return
+#     if message.content == 'Павло':
+#         await message.channel.send('саня')
+#
+#     if message.content == 'пидор':
+#         await message.channel.send('сам такой!')
 
 
-client = MyClient()
-client.run('')
+@client.command()
+async def latency(ctx): # простая комманда, чтобы узнать задержку бота (в развитии видимо)
+    await ctx.channel.send(f"Bots latency is around {round(client.latency * 1000)}ms")
+
+@client.command(aliases=["8ball"])
+async def _8ball(ctx, *, question): # рандомный ответ из 2-х (пока что) на любой вопрос
+    responses = ["NO FUCK YOU",
+                 "CMON LETS GO"]
+    await ctx.channel.send(f'{random.choice(responses)}')
+
+@client.command()
+async def add(ctx, left: int, right: int): # складываем два ЦЕЛЫХ числа
+    """Adds two numbers together."""
+    await ctx.channel.send(left + right)
+
+@client.command()
+async def clear(ctx, amount=3): # стираем сообщения в чате, по умолчанию будет стирать 3 сообщения
+    await ctx.channel.purge(limit=(amount + 1)) # +1 нужен. чтобы он ваш вызов .clear также стёр
+
+# client = MyClient()
+client.run('ODQzNTM1NDQxNTM4MTg3MzE1.YKFRmQ.8_dLFJTHG5_wKU4gVUSoV2r4-B4')
